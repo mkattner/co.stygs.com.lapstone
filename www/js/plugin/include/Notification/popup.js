4561b153c23b8ace0ac2da9e1c60927c3959fbe6
mkattnerBSB <martin.kattner@bsb-driveline.com>
2017-11-30 14:08:10 +0100

    add remove pupups

--- www/js/plugin/include/Notification/popup.js
+++ www/js/plugin/include/Notification/popup.js
@@ -1,2 +1,28 @@
-app.func("popup.open", function(templateId, templateElementsObject, buttonArray, callbackArray) {
+app.func("popup.open", function(templateId, templateElementsObject, buttonArray, callbackArray, $appendTo) {
+	var $popup;
+	$popup = app.template.get(templateId);
+
+	// append the loader to DOM
+	if ($appendTo !== undefined) {
+		$appendTo.append($popup);
+	} else {
+		$("div[data-role=content]").append($popup);
+	}
+
+	$popup.addClass("app-popup");
+
+	// REPLACE TEMPLATE ELEMENTS IN $popup
+	$.each(templateElementsObject, function(elementKey, elementValue) {
+		// REPLACE TEXT
+		if (typeof elementValue === "string")
+			$popup["_" + elementKey]().empty().text(elementValue);
+		// REPLACE OBJECT
+		else
+			$popup["_" + elementKey]().empty().append(elementValue);
+	});
+
 }, app.notify);
+
+app.func("popup.close", function() {
+	$(".app-popup").remove();
+}, app.notify)
