d26424c0b66c6447685b1461f91fb5a434387628
mkattnerBSB <martin.kattner@bsb-driveline.com>
2017-11-30 12:11:04 +0100

    add templateId as class to template html

--- www/js/plugin/plugin.HtmlTemplates.js
+++ www/js/plugin/plugin.HtmlTemplates.js
@@ -1,193 +1,218 @@
 // # sourceURL=plugin.HtmlTemplates.js
 /**
- * Copyright (c) 2015 martin.kattner@stygs.com Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify,
- * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
- * Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
- * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Copyright (c) 2015 martin.kattner@stygs.com Permission is hereby granted,
+ * free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions: The above copyright notice and this
+ * permission notice shall be included in all copies or substantial portions of
+ * the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+ * EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
  */
 
 var plugin_HtmlTemplates = {
-  config: null,
-  constructor: function() {
-    var dfd = $.Deferred();
-    dfd.resolve();
-    return dfd.promise();
-  },
-  pluginsLoaded: function() {
-    app.debug.trace("plugin_HtmlTemplates.pluginsLoaded()");
+	config : null,
+	constructor : function() {
+		var dfd = $.Deferred();
+		dfd.resolve();
+		return dfd.promise();
+	},
+	pluginsLoaded : function() {
+		app.debug.trace("plugin_HtmlTemplates.pluginsLoaded()");
 
-    var dfd = $.Deferred(), promises = Array(), promiseOfPromises;
+		var dfd = $.Deferred(), promises = Array(), promiseOfPromises;
 
-    if (app.config.min) {
-      dfd.resolve();
-    } else {
-      $.each(plugin_HtmlTemplates.config.templates, function(key, template) {
+		if (app.config.min) {
+			dfd.resolve();
+		} else {
+			$.each(plugin_HtmlTemplates.config.templates, function(key, template) {
 
-        app.debug.validate(template.content);
-        app.debug.validate(template.style);
+				app.debug.validate(template.content);
+				app.debug.validate(template.style);
 
-        promises.push(globalLoader.AsyncTextLoader(template.content));
-        promises.push(globalLoader.AsyncTextLoader(template.style));
+				promises.push(globalLoader.AsyncTextLoader(template.content));
+				promises.push(globalLoader.AsyncTextLoader(template.style));
 
-      });
+			});
 
-      promiseOfPromises = $.when.apply($, promises);
+			promiseOfPromises = $.when.apply($, promises);
 
-      promiseOfPromises.done(function() {
-        var args = arguments, argumentsIndex = 0;
+			promiseOfPromises.done(function() {
+				var args = arguments, argumentsIndex = 0;
 
-        $.each(plugin_HtmlTemplates.config.templates, function(key, template) {
+				$.each(plugin_HtmlTemplates.config.templates, function(key, template) {
 
-          // plugin_HtmlTemplates.config.templates[key] = {};
-          // console.log(i + args[i]);
-          plugin_HtmlTemplates.config.templates[key]['content'] = args[argumentsIndex];
-          argumentsIndex++;
+					// plugin_HtmlTemplates.config.templates[key] = {};
+					// console.log(i + args[i]);
+					plugin_HtmlTemplates.config.templates[key]['content'] = args[argumentsIndex];
+					argumentsIndex++;
 
-          plugin_HtmlTemplates.config.templates[key]['style'] = args[argumentsIndex];
+					plugin_HtmlTemplates.config.templates[key]['style'] = args[argumentsIndex];
 
-          argumentsIndex++;
-        });
+					argumentsIndex++;
+				});
 
-        dfd.resolve();
-      });
-      promiseOfPromises.fail(function() {
-        dfd.reject();
-      });
-    }
+				dfd.resolve();
+			});
+			promiseOfPromises.fail(function() {
+				dfd.reject();
+			});
+		}
 
-    return dfd.promise();
-  },
+		return dfd.promise();
+	},
 
-  // called after all pages are loaded
-  pagesLoaded: function() {
-    app.debug.trace("plugin_HtmlTemplates.pagesLoaded()");
-    var dfd = $.Deferred();
+	// called after all pages are loaded
+	pagesLoaded : function() {
+		app.debug.trace("plugin_HtmlTemplates.pagesLoaded()");
+		var dfd = $.Deferred();
 
-    dfd.resolve();
-    return dfd.promise();
-  },
+		dfd.resolve();
+		return dfd.promise();
+	},
 
-  definePluginEvents: function() {
-  },
+	definePluginEvents : function() {
+	},
 
-  // called by pages.js
-  afterHtmlInjectedBeforePageComputing: function(container) {
-    app.debug.trace("plugin_HtmlTemplates.afterHtmlInjectedBeforePageComputing()");
-  },
+	// called by pages.js
+	afterHtmlInjectedBeforePageComputing : function(container) {
+		app.debug.trace("plugin_HtmlTemplates.afterHtmlInjectedBeforePageComputing()");
+	},
 
-  pageSpecificEvents: function(container) {
-    app.debug.trace("plugin_HtmlTemplates.pageSpecificEvents()");
-  },
+	pageSpecificEvents : function(container) {
+		app.debug.trace("plugin_HtmlTemplates.pageSpecificEvents()");
+	},
 
-  parseLess: function(data) {
-    var css;
-    try {
-      less.render(data, {
-        filename: "../files/template/xxx.less"
-      }, function(error, output) {
-        css = output.css;
-        // output.imports = array of string filenames of the imports referenced
-      });
-    } catch (e) {
-      alert(e);
-    }
-    return css;
-  },
+	parseLess : function(data) {
+		var css;
+		try {
+			less.render(data, {
+				filename : "../files/template/xxx.less"
+			}, function(error, output) {
+				css = output.css;
+				// output.imports = array of string filenames of the imports
+				// referenced
+			});
+		} catch (e) {
+			alert(e);
+		}
+		return css;
+	},
 
-  getText: function(templateId) {
-    app.debug.trace("plugin_HtmlTemplates.getText()");
-    var text = null, style = null, styleIsActive;
+	getText : function(templateId) {
+		app.debug.trace("plugin_HtmlTemplates.getText()");
+		var text = null, style = null, styleIsActive;
 
-    text = plugin_HtmlTemplates.config.templates[templateId]['content'];
-    style = plugin_HtmlTemplates.config.templates[templateId]['style'];
+		// VALIDATE THE TEMPLATE
+		app.debug.validate(app.template.config.templates[templateId]);
+		app.debug.validate(app.template.config.templates[templateId]["content"]);
+		app.debug.validate(app.template.config.templates[templateId]["style"]);
 
-    if (!app.config.min) {
-      style = plugin_HtmlTemplates.parseLess(style);
-    }
+		text = plugin_HtmlTemplates.config.templates[templateId]["content"];
+		style = plugin_HtmlTemplates.config.templates[templateId]["style"];
 
-    styleIsActive = plugin_HtmlTemplates.config.templates[templateId]['styleIsActive'];
+		if (!app.config.min) {
+			style = plugin_HtmlTemplates.parseLess(style);
+		}
 
-    if (styleIsActive == undefined) {
-      styleIsActive = plugin_HtmlTemplates.config.templates[templateId]['styleIsActive'] = false;
-    }
+		styleIsActive = plugin_HtmlTemplates.config.templates[templateId]['styleIsActive'];
 
-    if ($("style#lapstoneStyles")[0] == undefined) {
-      $('head').append(function () { return $("<style>").attr("id", "lapstoneStyles")});
-    }
+		if (styleIsActive == undefined) {
+			styleIsActive = plugin_HtmlTemplates.config.templates[templateId]['styleIsActive'] = false;
+		}
 
-    if (styleIsActive === false) {
-      $("style#lapstoneStyles").append(style);
+		if ($("style#lapstoneStyles")[0] == undefined) {
+			$('head').append(function() {
+				return $("<style>").attr("id", "lapstoneStyles")
+			});
+		}
 
-      plugin_HtmlTemplates.config.templates[templateId]['styleIsActive'] = true;
+		if (styleIsActive === false) {
+			$("style#lapstoneStyles").append(style);
 
-    }
-    app.debug.debug("plugin_HtmlTemplates.getText() - text: " + text);
-    return text;
-  },
+			plugin_HtmlTemplates.config.templates[templateId]['styleIsActive'] = true;
 
-  getElements: function(templateId) {
-    app.debug.trace("plugin_HtmlTemplates.getElements()");
-    var elements;
+		}
+		app.debug.debug("plugin_HtmlTemplates.getText() - text: " + text);
+		return text;
+	},
 
-    elements = plugin_HtmlTemplates.config.templates[templateId]['elements'];
+	getElements : function(templateId) {
+		app.debug.trace("plugin_HtmlTemplates.getElements()");
+		var elements;
 
-    if (elements == undefined) elements = {};
+		elements = plugin_HtmlTemplates.config.templates[templateId]['elements'];
 
-    app.debug.debug("plugin_HtmlTemplates.getElements() - elements: " + JSON.stringify(elements));
+		if (elements == undefined)
+			elements = {};
 
-    return elements;
-  },
+		app.debug.debug("plugin_HtmlTemplates.getElements() - elements: " + JSON.stringify(elements));
 
-  functions: {
+		return elements;
+	},
 
-    get: function(templateId, templateObject) {
-      app.debug.trace("plugin_HtmlTemplates.functions.get()");
-      app.debug.debug("plugin_HtmlTemplates.functions.get() - templateId: " + templateId);
-      var extendObject;
+	functions : {
 
-      templateObject = templateObject || $(plugin_HtmlTemplates.getText(templateId));
-      extendObject = {};
+		get : function(templateId, $templateObject) {
+			app.debug.trace("plugin_HtmlTemplates.functions.get()");
+			app.debug.debug("plugin_HtmlTemplates.functions.get() - templateId: " + templateId);
+			var extendObject;
 
-      // extend the
-      if (plugin_HtmlTemplates.config.templates[templateId].elements) {
-        $.each(plugin_HtmlTemplates.config.templates[templateId].elements, function(elementKey, elementSelector) {
-          extendObject["_" + elementKey] = function(contentObject) {
-            if (contentObject) {
-              return this.find(elementSelector).append(contentObject);
-            }
+			$templateObject = $templateObject || $(plugin_HtmlTemplates.getText(templateId));
+			extendObject = {};
 
-            else {
-              return this.find(elementSelector);
-            }
-          }
+			// add templateId as class to $templateObject
+			if (!$templateObject.hasClass("template" + templateId))
+				$templateObject.addClass("template" + templateId);
 
-        });
-      }
+			// extend the jQuery template object with some element functions
+			app.debug.validate(app.template.config.templates[templateId]);
+			if (plugin_HtmlTemplates.config.templates[templateId].elements) {
+				$.each(plugin_HtmlTemplates.config.templates[templateId].elements, function(elementKey, elementSelector) {
+					extendObject["_" + elementKey] = function(contentObject) {
+						if (contentObject) {
+							return this.find(elementSelector).append(contentObject);
+						}
 
-      templateObject.extend(extendObject);
+						else {
+							return this.find(elementSelector);
+						}
+					}
 
-      return templateObject;
-    },
-    append: function(selector, templateId) {
-      app.debug.trace("plugin_HtmlTemplates.functions.append()");
-      $(selector).append(plugin_HtmlTemplates.functions.get(templateId));
-    },
-    prepend: function(selector, templateId) {
-      app.debug.trace("plugin_HtmlTemplates.functions.prepend()");
-      $(selector).prepend(plugin_HtmlTemplates.functions.get(templateId));
-    },
-    overwrite: function(selector, templateId) {
-      app.debug.trace("plugin_HtmlTemplates.functions.overwrite()");
-      app.debug.debug("plugin_HtmlTemplates.functions.overwrite() - selector: " + selector);
-      app.debug.debug("plugin_HtmlTemplates.functions.overwrite() - templateId: " + templateId);
-      $(selector).empty();
-      $(selector).attr("data-context", templateId);
-      $(selector).prepend(plugin_HtmlTemplates.functions.get(templateId));
-      app.debug.debug("plugin_HtmlTemplates.functions.overwrite() - new html code: " + $(selector)[0].outerHTML);
-    },
-    elements: function(templateId) {
-      app.debug.trace("plugin_HtmlTemplates.functions.elements()");
-      return plugin_HtmlTemplates.getElements(templateId);
-    }
-  }
+				});
+			}
+
+			$templateObject.extend(extendObject);
+
+			return $templateObject;
+		},
+		append : function(selector, templateId) {
+			app.debug.trace("plugin_HtmlTemplates.functions.append()");
+			$(selector).append(plugin_HtmlTemplates.functions.get(templateId));
+		},
+		prepend : function(selector, templateId) {
+			app.debug.trace("plugin_HtmlTemplates.functions.prepend()");
+			$(selector).prepend(plugin_HtmlTemplates.functions.get(templateId));
+		},
+		overwrite : function(selector, templateId) {
+			app.debug.trace("plugin_HtmlTemplates.functions.overwrite()");
+			app.debug.debug("plugin_HtmlTemplates.functions.overwrite() - selector: " + selector);
+			app.debug.debug("plugin_HtmlTemplates.functions.overwrite() - templateId: " + templateId);
+			$(selector).empty();
+			$(selector).attr("data-context", templateId);
+			$(selector).prepend(plugin_HtmlTemplates.functions.get(templateId));
+			app.debug.debug("plugin_HtmlTemplates.functions.overwrite() - new html code: " + $(selector)[0].outerHTML);
+		},
+		elements : function(templateId) {
+			app.debug.trace("plugin_HtmlTemplates.functions.elements()");
+			return plugin_HtmlTemplates.getElements(templateId);
+		}
+	}
 };
