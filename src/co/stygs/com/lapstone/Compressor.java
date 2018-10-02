806a2ac6580c9d11f73b2002b9149f268ca9c93f
Martin Kattner <martin.kattner@stygs.com>
2018-10-02 19:21:04 +0200

    ??

--- src/co/stygs/com/lapstone/Compressor.java
+++ src/co/stygs/com/lapstone/Compressor.java
@@ -19,102 +19,112 @@ import com.yahoo.platform.yui.compressor.YUICompressor;
 
 public class Compressor {
 
-	public static Logger logger = Logger.getLogger(YUICompressor.class.getName());
+    public static Logger logger = Logger.getLogger(YUICompressor.class.getName());
 
-	private static class YuiCompressorErrorReporter implements ErrorReporter {
-		@Override
-		public void warning(String message, String sourceName, int line, String lineSource, int lineOffset) {
-			if (line < 0) {
-				logger.log(Level.WARNING, message);
-			} else {
-				logger.log(Level.WARNING, line + ':' + lineOffset + ':' + message);
-			}
-		}
-
-		@Override
-		public void error(String message, String sourceName, int line, String lineSource, int lineOffset) {
-			if (line < 0) {
-				logger.log(Level.SEVERE, message);
-			} else {
-				logger.log(Level.SEVERE, line + ":" + lineOffset + ":" + message);
-			}
-		}
-
-		@Override
-		public EvaluatorException runtimeError(String message, String sourceName, int line, String lineSource, int lineOffset) {
-			error(message, sourceName, line, lineSource, lineOffset);
-			return new EvaluatorException(message);
-		}
+    private static class YuiCompressorErrorReporter implements ErrorReporter {
+	@Override
+	public void warning(String message, String sourceName, int line, String lineSource, int lineOffset) {
+	    if (line < 0) {
+		logger.log(Level.WARNING, message);
+	    } else {
+		logger.log(Level.WARNING, line + ':' + lineOffset + ':' + message);
+	    }
 	}
 
-	public static class JavascriptCompressorOptions {
-		public String charset = "UTF-8";
-		public int lineBreakPos = -1;
-		public boolean munge = true;
-		public boolean verbose = true;
-		public boolean preserveAllSemiColons = false;
-		public boolean disableOptimizations = false;
-
-		public JavascriptCompressorOptions() {
-		}
-
-		public JavascriptCompressorOptions(Boolean verbose) {
-			this.verbose = verbose;
-		}
+	@Override
+	public void error(String message, String sourceName, int line, String lineSource, int lineOffset) {
+	    if (line < 0) {
+		logger.log(Level.SEVERE, message);
+	    } else {
+		logger.log(Level.SEVERE, line + ":" + lineOffset + ":" + message);
+	    }
 	}
 
-	public static class StylesheetCompressorOptions {
-		public String charset = "UTF-8";
+	@Override
+	public EvaluatorException runtimeError(String message, String sourceName, int line, String lineSource, int lineOffset) {
+	    error(message, sourceName, line, lineSource, lineOffset);
+	    return new EvaluatorException(message);
+	}
+    }
+
+    public static class JavascriptCompressorOptions {
+	public String charset = "UTF-8";
+	public int lineBreakPos = -1;
+	public boolean munge = true;
+	public boolean verbose = true;
+	public boolean preserveAllSemiColons = false;
+	public boolean disableOptimizations = false;
+
+	public JavascriptCompressorOptions() {
 	}
 
-	public static void compressJavaScript(String inputFilename, String outputFilename, JavascriptCompressorOptions o) throws Exception {
+	public JavascriptCompressorOptions(Boolean verbose) {
+	    this.verbose = verbose;
+	}
+    }
 
-		Reader in = null;
-		Writer out = null;
+    public static class StylesheetCompressorOptions {
+	public String charset = "UTF-8";
+    }
 
-		try {
+    public static void compressJavaScript(String inputFilename, String outputFilename, JavascriptCompressorOptions o) throws Exception {
 
-			System.out.println();
-			System.out.println("Compress js: " + inputFilename);
-			System.out.println("  Rename to: " + outputFilename);
+	Reader in = null;
+	Writer out = null;
 
-			in = new InputStreamReader(new FileInputStream(inputFilename), o.charset);
+	try {
 
-			JavaScriptCompressor compressor = new JavaScriptCompressor(in, new YuiCompressorErrorReporter());
-			in.close();
-			in = null;
+	    System.out.println();
+	    System.out.println("Compress js: " + inputFilename);
+	    System.out.println("  Rename to: " + outputFilename);
 
-			out = new OutputStreamWriter(new FileOutputStream(outputFilename), o.charset);
-			compressor.compress(out, o.lineBreakPos, o.munge, o.verbose, o.preserveAllSemiColons, o.disableOptimizations);
-		}
+	    in = new InputStreamReader(new FileInputStream(inputFilename), o.charset);
 
-		finally {
-			IOUtils.closeQuietly(in);
-			IOUtils.closeQuietly(out);
-		}
+	    JavaScriptCompressor compressor = new JavaScriptCompressor(in, new YuiCompressorErrorReporter());
+	    in.close();
+	    in = null;
+
+	    out = new OutputStreamWriter(new FileOutputStream(outputFilename), o.charset);
+	    compressor.compress(out, o.lineBreakPos, o.munge, o.verbose, o.preserveAllSemiColons, o.disableOptimizations);
 	}
 
-	public static void compressStylesheet(String inputFilename, String outputFilename, StylesheetCompressorOptions o) throws Exception {
-
-		Reader in = null;
-		Writer out = null;
-
-		try {
-			in = new InputStreamReader(new FileInputStream(inputFilename), o.charset);
-
-			CssCompressor compressor = new CssCompressor(in);
-
-			in.close();
-			in = null;
-
-			out = new OutputStreamWriter(new FileOutputStream(outputFilename), o.charset);
-			compressor.compress(out, 0);
-		}
-
-		finally {
-			IOUtils.closeQuietly(in);
-			IOUtils.closeQuietly(out);
-		}
+	catch (Exception e) {
+	    e.printStackTrace();
+	    throw new Exception();
 	}
+
+	finally {
+	    IOUtils.closeQuietly(in);
+	    IOUtils.closeQuietly(out);
+	}
+    }
+
+    public static void compressStylesheet(String inputFilename, String outputFilename, StylesheetCompressorOptions o) throws Exception {
+
+	Reader in = null;
+	Writer out = null;
+
+	try {
+	    in = new InputStreamReader(new FileInputStream(inputFilename), o.charset);
+
+	    CssCompressor compressor = new CssCompressor(in);
+
+	    in.close();
+	    in = null;
+
+	    out = new OutputStreamWriter(new FileOutputStream(outputFilename), o.charset);
+	    compressor.compress(out, 0);
+	} 
+	
+	catch (Exception e) {
+	    e.printStackTrace();
+	    throw new Exception();
+	}
+
+	finally {
+	    IOUtils.closeQuietly(in);
+	    IOUtils.closeQuietly(out);
+	}
+    }
 
 }
