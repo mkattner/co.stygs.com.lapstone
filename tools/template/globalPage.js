ba41c5b7f56699e91e7b34d630433be89f8cb54a
Martin Kattner <martin.kattner@stygs.com>
2018-11-28 19:13:39 +0100

    template

---
+++ tools/template/globalPage.js
@@ -0,0 +1,103 @@
+// # sourceURL=globalPage.##template.js
+var globalPage_##template = {
+
+  elements: {},
+
+  /*
+   * jQuery mobile event - pagebeforechange
+   */
+  pagebeforechange: function(event, $container) {
+    app.debug.event(event);
+  },
+
+  /*
+   * jQuery mobile event - pagebeforecreate
+   */
+  pagebeforecreate: function(event, $container) {
+    app.debug.event(event);
+  },
+
+  /*
+   * jQuery mobile event - pagebeforehide
+   */
+  pagebeforehide: function(event, $container) {
+    app.debug.event(event);
+  },
+
+  /*
+   * jQuery mobile event - pagebeforeload
+   */
+  pagebeforeload: function(event, $container) {
+    app.debug.event(event);
+  },
+
+  /*
+   * jQuery mobile event - pagebeforeshow
+   */
+  pagebeforeshow: function(event, $container) {
+    app.debug.event(event);
+  },
+
+  /*
+   * jQuery mobile event - pagechange
+   */
+  pagechange: function(event, $container) {
+    app.debug.event(event);
+  },
+
+  /*
+   * jQuery mobile event - pagechangefailed
+   */
+  pagechangefailed: function(event, $container) {
+    app.debug.event(event);
+  },
+
+  /*
+   * jQuery mobile event - pagecreate
+   */
+  pagecreate: function(event, $container) {
+    app.debug.event(event);
+  },
+
+  /*
+   * jQuery mobile event - pagehide
+   */
+  pagehide: function(event, $container) {
+    app.debug.event(event);
+  },
+
+  /*
+   * jQuery mobile event - pageinit
+   */
+  pageinit: function(event, $container) {
+    app.debug.event(event);
+  },
+
+  /*
+   * jQuery mobile event - pageload
+   */
+  pageload: function(event, $container) {
+    app.debug.event(event);
+  },
+
+  /*
+   * jQuery mobile event - pageloadfailed
+   */
+  pageloadfailed: function(event, $container) {
+    app.debug.event(event);
+  },
+
+  /*
+   * jQuery mobile event - pageremove
+   */
+  pageremove: function(event, $container) {
+    app.debug.event(event);
+  },
+
+  /*
+   * jQuery mobile event - pageshow
+   */
+  pageshow: function(event, $container) {
+    app.debug.event(event);
+  }
+}
